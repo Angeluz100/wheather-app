d903efe7ac816bcafa7b67e13fcf17b90759c497
Angeluz100 <francotgutierrez@gmail.com>
2021-06-21 15:03:45 -0400

    api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+const api = {
+  key: "b55281e8bfefd1458ae4dadcd74cf082",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div className="App">
-        Hello Franco
+    <div className="app">
+        <main>
+          <div className="search-box">
+            <input 
+            type="text"
+            className="search-bar"
+            placeholder="search..."
+            />
+          </div>
+        </main>
     </div>
   );
 }

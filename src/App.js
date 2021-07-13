31ee9eadc030a0991c1fbb35e260b09a74b3d212
Angeluz100 <francotgutierrez@gmail.com>
2021-07-13 11:28:51 -0400

    Debugging

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const search = evt => {
     if(evt.key === "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setWeather(result)
@@ -33,7 +33,7 @@ function App() {
 
 
   return (
-    <div className={(typeof weather.main != 'undefined') ?  ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != 'undefined') ?  ((weather.main.temp > 60) ? 'app warm' : 'app') : 'app'}>
         <main>
           <div className="search-box">
             <input 
@@ -53,7 +53,7 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
               <div className="weather-box">
                 <div className="temp">
-                  {Math.round(weather.main.temp)}°c
+                  {Math.round(weather.main.temp)}°F
                 </div>
                 <div className="weather">{weather.weather[0].main}</div>
               </div>
